9a345c5bf257d7124c243e981a51821927526e88
Jane Venter <janeanneventer@gmail.com>
2018-10-14 17:31:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(int)
   if int <= 1
    false
   end
-  ar = (2..int-1).to_a
-  ar.each do |i|
+ # ar = (2..int-1).to_a
+  for i in 2..int-1 do
     if int % i == 0 
       false
     else

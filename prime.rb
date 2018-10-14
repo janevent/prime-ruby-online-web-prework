11d44df036f928e6b689fe9b962fee579a2550f8
Jane Venter <janeanneventer@gmail.com>
2018-10-14 17:09:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ def prime?(int)
  # if int == 1 || 0
   #  false
   #end
-  Math.sqrt(int).floor.downto(2).each {|i| return false if int % i == 0}
+  #Math.sqrt(int).floor.downto(2).each {|i| return false if int % i ##== 0}
   true
 end

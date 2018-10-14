816ee79e41b46789a18f989440b17efc025ef93e
Jane Venter <janeanneventer@gmail.com>
2018-10-14 17:40:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(int)
  # ar = (2..int-1).to_a
  numbers = 2..int-1
   for i in numbers
-    if int % counter == 0 
+    if int % i == 0 
       false
     else
   #Math.sqrt(int).floor.downto(2).each {|i| return false if int % i ##== 0}
